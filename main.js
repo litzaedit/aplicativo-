a6a250275f95a8355df95937977e8b48313a83f5
mariana <103067658+litzaedit@users.noreply.github.com>
2023-11-24 08:53:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 function tocaSomPom (){
-document.querySelector('#som_tecla_pom').play();
+    document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelectorAll ('.tecla')
+
+    const listaDeTeclas = document.querySelectorAll ('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+let contador = 0;
+
+//enquanto
+while (contador = 9)  {
+    
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
